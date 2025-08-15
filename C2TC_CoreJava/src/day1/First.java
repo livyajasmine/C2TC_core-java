0ab4a8d9ff8f034ae216e69d140f153cafaec1e1
LIVYA JASMINE <LIVYA JASMINE@RITHIKA007>
2025-08-15 16:10:45 +0530

    day1

--- C2TC_CoreJava/src/day1/First.java
+++ C2TC_CoreJava/src/day1/First.java
@@ -1,10 +1,9 @@
-package C2TC_CoreJava;
+package day1;
 
-public class day1 {
-
+public class First {
 	public static void main(String[] args) {
-		
+			
 		System.out.println("Hello World!");
-	}
+		}
 
 }
