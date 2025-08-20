8c604699dc97f5ba2fb106c9b1e08f5359c4282a
livyajasmine <livyajasmine140@gmail.com>
2025-08-20 18:02:13 +0530

    day6

---
+++ C2TC_CoreJava/src/day6/usingfinal/FinalClass.java
@@ -0,0 +1,17 @@
+package day6.usingfinal;
+
+final class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+
+
+// ❌ This will cause error because FinalClass is final
+// class FinalChildClass extends FinalClass { }
+
+//public class FinalClass {
+	public static void main(String[] args) {
+		FinalClass f1 = new FinalClass();
+		f1.show();
+	}
+}
